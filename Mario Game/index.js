9fb513dd710e3001425c3c1761bc68bf59c1afcb
kobohuong <kobohuong@gmail.com>
2022-01-15 21:18:52 +0100

    init

---
+++ Mario Game/index.js
@@ -0,0 +1,71 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+const gravity = 1.5
+
+class Player {
+    constuctor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.width = 30
+        this.height = 30
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+            this.velocity.y += gravity
+        else this.velocity.y = 0
+    }
+
+}
+
+const player = new Player()
+player.draw()
+
+function animate() {
+    requestAnimationFrame(animate)
+    console.log('go')
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+}
+animate()
+addEventListener('keydown', ({ keyCode }) => {
+    console.log(keyCode)
+    switch (keyCode) {
+        case 65:
+            console.log('left')
+            break
+
+        case 83:
+            console.log('down')
+            break
+
+        case 68:
+            console.log('right')
+            break
+
+        case 87:
+            console.log('up')
+            player.velocity.y− = 20
+            break
+
+
+
+
+    }
+})
